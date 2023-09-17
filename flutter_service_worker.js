5ccc1f7a6fba92042a993af21c73b6842d22dd11
johnloser-lwi <johnloser-lwi@users.noreply.github.com>
2023-09-17 03:34:03 +0000

    deploy: c2c24a355d0dd90e0f3588141da5e30a84b576cc

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"index.html": "654b11f0e6f45b56f52b886c61a9290c",
-"/": "654b11f0e6f45b56f52b886c61a9290c",
+"index.html": "f836a866fc8513e6309053fb8096f4c7",
+"/": "f836a866fc8513e6309053fb8096f4c7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "569ee53f429e4f7fd37d76a5b3aceceb",
 "manifest.json": "12c31ce892e3811f4657fbde0450f3f7"};
